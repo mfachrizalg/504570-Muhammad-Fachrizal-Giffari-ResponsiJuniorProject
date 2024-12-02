9d6f8c4b9dca1627ed0ac7010597bb26294fca9f
Muhammad Fachrizal Giffari <muhammad.fachrizal.giffari@mail.ugm.ac.id>
2024-12-02 08:00:51 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _504570_Muhammad_Fachrizal_Giffari_ResponsiJuniorProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace _504570_Muhammad_Fachrizal_Giffari_ResponsiJuniorProject
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
